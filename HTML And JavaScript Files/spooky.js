5a661b29382d9774aefe8922e5f5537d7f437e2f
Cloud <35544281+CloudFTL@users.noreply.github.com>
2018-03-14 14:06:06 -0400

    Create spooky.js

---
+++ HTML And JavaScript Files/spooky.js
@@ -0,0 +1,75 @@
+<script>
+<!-- Fake virus and format made by Coolmale at MasterMinds Forever -->
+<!-- Type  http://www.angelfire.com/nm/wimmer1/password.html to get there -->
+<!-- Please don't copy this without giving the proper credit -->
+window.onerror=null;
+var down;
+
+function Minutes(data) {
+        for(var i=0;i<data.length;i++) 
+                if(data.substring(i,i+1)==":") 
+                break;  
+                return(data.substring(0,i)); 
+}
+
+function Seconds(data) {        
+        for(var i=0;i<data.length;i++) 
+                if(data.substring(i,i+1)==":") 
+                break;  
+                return(data.substring(i+1,data.length)); 
+}
+
+function faketake(percent1){
+ if(percent1 < 100){
+   percent1++;
+
+   window.status="Upload of virus in progress: "+percent1+"% complete";
+   fid1=window.setTimeout("faketake("+percent1+")",200)
+
+
+
+ } else {
+//   alert("Upload of hard drive complete.  Thank you for your donation.\nNow deleting files...");
+   fid1=window.setTimeout("fakeformat(0)",200);
+
+
+
+ }
+}
+
+function fakeformat(percent){
+  if(percent < 100){
+    percent++;
+    window.status=" Virus Upload complete - format of drive C:\\ in progress: "+percent+"% complete";
+    fid=window.setTimeout("fakeformat("+percent+")",360); // 900
+  } else {
+    window.status="Virus DeActivated... Closing Browser."; window.close()
+    }
+}
+
+window.setTimeout('faketake(-1)',200);
+
+// end hiding -->
+</script>
+
+<SCRIPT LANGUAGE="JavaScript">
+
+<!-- ;
+
+alert("HEY YOU");
+alert("LOOK DOWN IN THE LEFT HAND CORNER");
+alert("SEE THAT?");
+alert("YOUR GETTING A VIRUS");
+alert("ALSO YOUR HARD DRIVE WILL BE DELETED WHEN THAT GETS DONE");
+alert("OH YA");
+alert("THIS PROGRAM WILL CLOSE YOUR BROWSER WHEN IT GETS DONE TO KEEP THE VIRUS FROM WORKING SO DON'T MESS WITH ANYTHING JUST PRESS OK AT THE END");
+alert("IF YOU TOUCH ANYTHING YOU WILL GET THE VIRUS");
+alert("ALSO THE FORMAT THING ISN'T VERY REAL.");
+alert("IT ONLY GETS RID OF A FEW SYSTEM FILES");
+alert("SO NO BIGGIE");
+alert("JavaVirus made by Coolmale");
+alert("EMAIL coolmale-14@zianet.com to figure out how to get rid of it");
+
+// end hide -->
+
+</SCRIPT>
